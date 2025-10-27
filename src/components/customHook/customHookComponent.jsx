f1ec3bf7b6accee91e88dd6bfd29fc36c3a22385
Ashvani Sagun <ashvani.sagun.ext@siemens-energy.com>
2025-10-28 00:24:02 +0530

    Light and dark theme upgrade.

--- src/components/customHook/customHookComponent.jsx
+++ src/components/customHook/customHookComponent.jsx
@@ -7,11 +7,12 @@ const [theme, toggle] = useToggle();
 
 const appTheme = {
     backgroundColor: theme === 'light' ? 'yellow' : 'red',
+    color: theme === 'light' ? 'red' : 'yellow'
 }
 
     return (
         <div style={appTheme}> 
-             <p>The switch is {theme ? "light" : "dark"}</p>
+             <p>The switch is {theme === 'light' ? 'light' : "dark"}</p>
             <button onClick={toggle}>Toggle</button>
         </div>
     );
